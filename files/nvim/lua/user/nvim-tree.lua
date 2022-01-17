5b457e9c08f53f6c46370a698e26f9cb11b7b59b
vecin2 <vecin2@gmail.com>
2022-01-17 16:07:20 -0500

    fix conflict between nvim-tree and gx

--- files/nvim/lua/user/nvim-tree.lua
+++ files/nvim/lua/user/nvim-tree.lua
@@ -36,8 +36,8 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
+  disable_netrw = false, --otherwise gx does not work in WSL
+  hijack_netrw = false,
   open_on_setup = false,
   ignore_ft_on_setup = {
     "startify",
