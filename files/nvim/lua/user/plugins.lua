bf9b799bb52985cff258c81fa846f610fb91fd2c
davgarciawork <david.avgarcia.work@gmail.com>
2022-03-18 22:45:10 -0400

    add handlebars nvim plugin

--- files/nvim/lua/user/plugins.lua
+++ files/nvim/lua/user/plugins.lua
@@ -87,7 +87,9 @@ return packer.startup(function(use)
 	use("L3MON4D3/LuaSnip") --snippet engine
 	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 	--}}}
-
+	--Javascript{{{{
+	use('mustache/vim-mustache-handlebars')
+	--}
 	-- cmp plugins{{{
 	use("hrsh7th/nvim-cmp") -- The completion plugin
 	use("hrsh7th/cmp-buffer") -- buffer completions
