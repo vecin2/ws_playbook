75b3cb5518eca688655d3f0807a002f362020512
davgarciawork <david.avgarcia.work@gmail.com>
2022-03-18 22:34:13 -0400

    avoid prompt on saving py files (formatting)

--- files/nvim/lua/user/lsp/handlers.lua
+++ files/nvim/lua/user/lsp/handlers.lua
@@ -86,7 +86,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
+  if client.name == "tsserver" or client.name== "pylsp" then
     client.resolved_capabilities.document_formatting = false
   end
   lsp_keymaps(bufnr)
