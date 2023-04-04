8ad615668266c5111230c5cc407b3bf433bfc8ed
vecin2 <vecin2@gmail.com>
2023-04-04 23:10:26 +0200

    disable autoformat on save

--- files/nvim/lua/user/lsp/null-ls.lua
+++ files/nvim/lua/user/lsp/null-ls.lua
@@ -23,16 +23,16 @@ null_ls.setup({
 		code_actions.refactoring,
 	},
 	-- auto-format on save
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format()
-				end,
-			})
-		end
-	end,
+	--[[ on_attach = function(client, bufnr) ]]
+	--[[ 	if client.supports_method("textDocument/formatting") then ]]
+	--[[ 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr }) ]]
+	--[[ 		vim.api.nvim_create_autocmd("BufWritePre", { ]]
+	--[[ 			group = augroup, ]]
+	--[[ 			buffer = bufnr, ]]
+	--[[ 			callback = function() ]]
+	--[[ 				vim.lsp.buf.format() ]]
+	--[[ 			end, ]]
+	--[[ 		}) ]]
+	--[[ 	end ]]
+	--[[ end, ]]
 })
