213a7a6642418f525052434d449bb12e4f5ba292
vecin2 <vecin2@gmail.com>
2024-03-08 06:37:59 +0100

    readd ansible repository

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,3 @@
 sudo apt update
-sudo apt install software-properties-common
-#No need to add repostory in ubuntu 20.04
-#sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
