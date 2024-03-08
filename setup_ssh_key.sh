4d6ea4102d2be75fc3e67dbce7d444b394d2b2c0
vecin2 <vecin2@gmail.com>
2024-03-08 06:47:15 +0100

    add ssh config

--- setup_ssh_key.sh
+++ setup_ssh_key.sh
@@ -6,16 +6,16 @@ agent_start () {
 	(umask 077; ssh-agent >| "$env")
 	. "$env" >| /dev/null ; }
 
-	agent_load_env
+agent_load_env
 
-		# agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
-		agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
+# agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
+agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
 
-		if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
-			agent_start
-			ssh-add
-		elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
-			ssh-add
-		fi
+if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+	agent_start
+	ssh-add $1
+elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
+	ssh-add $1
+fi
 
-		unset env
+unset env
