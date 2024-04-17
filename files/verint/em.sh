c702d952e7f8c36c8ef7f34dca7727e494d8618d
vecin2 <vecin2@gmail.com>
2024-04-17 12:56:45 +0200

    adding upgrade EM compare commands

--- files/verint/em.sh
+++ files/verint/em.sh
@@ -1,7 +1,11 @@
 #configurable vars
-export EM_CORE_HOME=/mnt/c/em/projects/sgroup/2024_Upgrade/
+export EM_CORE_HOME=/mnt/c/em/projects/sgroup/2024_Upgrade
 export SQL_TEMPLATES_PATH=C:\\ProgramData\\Verint\\sqltask_templates\\templates
-export PRODUCT_HOME=/mnt/c/em/products/agent-desktop_15.3-2021R4-HFR_7.4.4
+export PRODUCT_HOME=/mnt/c/em/products/agent-desktop_2023R4-HFR_9.4.5
+export OLD_PRODUCT_HOME=/mnt/c/em/products/agent-desktop_15.3-2021R4-HFR_7.4.4
+export PROJECT_PREFIX="SGroup"
+
+
 dbtype=sqlserver #oracle
 
 #Start EM functions
@@ -45,9 +49,15 @@ val(){
 }
 
 scanlogs(){
+	if [ "$#" -eq 1 ];
+	then
+		no_of_files=$1
+	else
+		no_of_files=25
+	fi
 	dir=$EM_CORE_HOME/logs
 
-	find $dir -type f -exec stat --format '%Y :%y %n' "{}" \; | sort -nr | cut -d: -f2- | head -n 25
+	find $dir -type f -exec stat --format '%Y :%y %n' "{}" \; | sort -nr | cut -d: -f2- | head -n $no_of_files
 }
 
 ccadmin(){
@@ -95,16 +105,16 @@ recreate_em_app(){
 find_jar_by_classname(){
 	find . -name '*.jar' -exec grep -Hls $1 {} \;
 }
-custdiff(){
+diffrepofile(){
 	prj_path=$(fzf)
 	full_prj_path=$(pwd)/"${prj_path}"
 
 	classpath="${full_prj_path##*/repository/default/}"
 
-	query="${classpath##PC}" #remove project prefix from path
+	query="${classpath#"$PROJECT_PREFIX"}" #remove project prefix from path
 
 
-	product_path="${PRODUCT_HOME}"/repository/default
+	product_path="${product_home}"/repository/default
 
 	full_product_path=$(find "${product_path}" | fzf -q "${query}")
 
@@ -113,8 +123,109 @@ custdiff(){
 		echo Comparing "${full_prj_path}" with "${full_product_path}"
 		vimdiff "${full_prj_path}" "${full_product_path}"
 	fi
+}
+function three_way_file_merge_usage {
+    echo ""
+    echo "Allows comparing files from EM project, EM product and EM old product"
+    echo ""
+    echo "usage: $0 --directory string --force-copy --line-regex string"
+    echo ""
+    echo "  -d  string  name of the service"
+    echo "              (example: config)"
+    echo "  -f	flag    override file in merge folder"
+    echo "  -r  string  Removes all lines that do no match this regex"
+		echo "              (example: policy)"
+    echo ""
+}
+three_way_file_merge(){
+	local project_top_folder=""
+	local force_copy=""
+	local line_regex=""
+	local invalid_param=""
+		
+	while getopts ":d:f:r:" opt; do
+		case $opt in
+			d) project_top_folder=$OPTARG;;
+			f) force=true;;
+			r) line_regex=$OPTARG;;
+			\?) usage
+					return 1;;
+		esac
+	done
+	three_way_finder $project_top_folder
+	if [ -z "${project_file_path}" ]; then
+		return 1
+	fi
+	if [ -z "${product_file_path}" ]; then
+		return 1
+	fi
+	merge_folder=$EM_CORE_HOME/"merge"
+	merge_subfolders=$(dirname ${project_file_path#"$EM_CORE_HOME/"})
+	merge_path=$merge_folder/$merge_subfolders
 
+	otb_file_prefix=$([ -z "$line_regex" ] && echo "otb_" || echo "otb_"$line_regex"_") 
+	merge_otb_file=$merge_path/"$otb_file_prefix"$(basename $product_file_path)
+	copy_file_if_not_exist "$product_file_path" "$merge_otb_file" "$force"
 
+	project_file_prefix=$([ -z "$line_regex" ] && echo "project_" || echo "project_"$line_regex"_") 
+	merge_project_file=$merge_path/"$project_file_prefix"$(basename $product_file_path)
+	copy_file_if_not_exist "$project_file_path" "$merge_project_file" "$force"
+
+	product_relative_path=${product_file_path#"$PRODUCT_HOME/"}
+	oldotb_file_prefix=$([ -z "$line_regex" ] && echo "oldotb_" || echo "oldotb_"$line_regex"_") 
+	merge_oldotb_file=$merge_path/"$oldotb_file_prefix"$(basename $product_file_path)
+	copy_file_if_not_exist "$OLD_PRODUCT_HOME"/"$product_relative_path" "$merge_oldotb_file" "$force"
+
+	if [ ! -z "$line_regex" ]; then
+		 sed -i "/$line_regex/!d" "$merge_otb_file"
+		 sed -i "/$line_regex/!d" "$merge_project_file"
+		 sed -i "/$line_regex/!d" "$merge_oldotb_file"
+	fi
+
+	kvim -d "$merge_otb_file" "$merge_oldotb_file" "$merge_project_file"
+}
+copy_file_if_not_exist(){
+	if [[ $# -ne 2 && $# -ne 3 ]]; then
+		return 1
+	fi
+	src_file=$1
+	dst_file=$2
+	force_copy=$3
+	
+
+	if [[ ! -f "${dst_file}" || "$force_copy" ]]; then
+		echo Creating "$dst_file"
+		echo mkdir -p $(dirname "$dst_file")
+		mkdir -p $(dirname "$dst_file")
+		echo "$src_file" copied to "$dst_file"
+		cp "$src_file" "$dst_file"
+	fi
+}
+
+three_way_finder(){
+	if [ $# -eq 1 ]; then
+		project_top_folder=$1
+	else
+		project_top_folder=""
+	fi
+	# prj_path=$(fzf)
+	fzf_path=${EM_CORE_HOME}/$project_top_folder
+	project_file_path=$(fd . $fzf_path | fzf)
+
+	query=$(basename "$project_file_path") #query fzf using the file name on the same top folder
+
+	fzf_product_path="${PRODUCT_HOME}"/$project_top_folder
+
+	if [ -z "${project_file_path}" ]; then
+		return 1
+	fi
+	product_file_path=$(fd . "${fzf_product_path}" | fzf -q "${query}")
+	#
+	#
+	# if [ -f  "${full_prj_path}" ] && [ -f "${full_product_path}" ]; then
+	# 	echo Comparing "${full_prj_path}" with "${full_product_path}"
+	# 	vimdiff "${full_prj_path}" "${full_product_path}"
+	#fi
 }
 install_ccadmin_autocompletion(){
 	if [ -z $EM_CORE_HOME ]; then
@@ -206,8 +317,48 @@ restore_db_snapshot(){
 
 
 #create container with backup data
-echo "ReCreating container $container_name from image $snapshot_file_name"
-/home/dgarcia/dev/docker/mydocker-cmds/${dbtype}19/create-docker-database.sh $container_name $port ${snapshots_path}/${snapshot_file_name}
+echo "Recreating container $container_name from image ${snapshots_path}/${snapshot_file_name}"
+/home/dgarcia/dev/docker/mydocker-cmds/${dbtype}19/run_container_from_tar.sh $container_name $port ${snapshots_path}/${snapshot_file_name}
 echo "Starting container " $container_name
 docker start $container_name
+}
+
+upgrade_patches(){
+	if [ "$#" -ne 1 ]; then
+		echo "Usage $0 <<version_number>>, e.g. $0 9.1.4"
+		return 1
+	fi
+	#create new product.version file
+	curdir=$(pwd)
+	mkdir -p /tmp/META-INF
+	echo "$1" > /tmp/META-INF/product.version
+	cd /tmp
+	for file in $EM_CORE_HOME/patches/*.zip
+	do
+		zip -u "$file" META-INF/product.version
+	done
+	rm -rf /tmp/META-INF
+	cd "$curdir"
+}
+upgrade_migration_jars(){
+	if [ "$#" -ne 1 ]; then
+		echo "Usage $0 <<version_number>>, e.g. $0 9.1.4"
+		return 1
+	fi
+	version=$1
+
+	for migration_jar in $EM_CORE_HOME/migration/*.jar; do
+		# unzip migration_jar -d 
+		# echo $migration_jar
+		dest_folder=${migration_jar%.*}
+		echo unzip $migration_jar -d $dest_folder
+		unzip $migration_jar -d $dest_folder
+		find $dest_folder -type f -exec sed -i "s/\"VERSION\" : \".*\",/\"VERSION\" : \"$version\",/" {} +
+		jar_filename=$(basename $migration_jar)
+		echo "(cd $dest_folder && zip -r ../$jar_filename)"
+
+		(cd $dest_folder && zip -r --freshen ../$jar_filename .)
+		echo rm -r $dest_folder
+		rm -r $dest_folder
+	done
 }
