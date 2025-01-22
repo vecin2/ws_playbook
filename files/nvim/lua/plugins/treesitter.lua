c9fddefd808d62d639e31c83fc63d43bb8fff297
vecin2 <vecin2@gmail.com>
2025-01-22 23:00:30 +0100

    update treesitter config

--- files/nvim/lua/plugins/treesitter.lua
+++ files/nvim/lua/plugins/treesitter.lua
@@ -13,9 +13,13 @@ return {
     treesitter.setup({ -- enable syntax highlighting
       highlight = {
         enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
       },
       -- enable indentation
-      indent = { enable = true },
+      indent = { enable = true, disable = { 'ruby' } },
       -- enable autotagging (w/ nvim-ts-autotag plugin)
       autotag = {
         enable = true,
@@ -41,6 +45,9 @@ return {
         "c",
 				"python"
       },
+
+      -- Autoinstall languages that are not installed
+			auto_install =true,
       incremental_selection = {
         enable = true,
         keymaps = {
