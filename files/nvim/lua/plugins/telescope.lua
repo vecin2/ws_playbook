3c711b8e0a21f6c43c39604e6dacdcf93f642dde
vecin2 <vecin2@gmail.com>
2025-01-23 00:40:00 +0100

    fix issue with telescope

--- files/nvim/lua/plugins/telescope.lua
+++ files/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,3 @@
-local actions = require "telescope.actions"
-
-local keymap = vim.keymap --for conciseness
-local builtin = require "telescope.builtin"
-
-local keys = {
-	{ "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer search" },
-	{ "<leader>b", function() builtin.buffers({ ignore_current_buffer = true, sort_mru = true, }) end, desc = "Buffers" },
-	{ "<leader>t", "<cmd>Telescope find_files<cr>", desc = "Find All Files" },
-	{ "<C-p>", "<cmd>Telescope git_files<cr>", desc = "Git files" },
-	{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
-	{ "<leader>sr", "<cmd>Telescope command_history<cr>", desc = "History" },
-	{ "<leader>sc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
-	{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
-	{ "<leader>sl", "<cmd>Telescope lsp_references<cr>", desc = "Lsp References" },
-	{ "<leader>so", "<cmd>Telescope oldfiles<cr>", desc = "Old files" },
-	{ "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Ripgrep" },
-	{ "<leader>ag", "<cmd>Telescope grep_string<cr>", desc = "Grep String" },
-	{ "<leader>st", "<cmd>Telescope treesitter<cr>", desc = "Treesitter" },
-	{ "<leader>md", function() builtin.find_files({ cwd = "$DOT_FILES_LOC", prompt_title = "<MYVIMRC>" }) end, desc = "Find my dot files" },
-	{ "<leader>ss", builtin.builtin, desc = "[Search] [S]elect Telescope" },
-	{ "<leader>ss", builtin.diagnostics, desc = "[Search] [D]iagnostics" },
-}
 
 local config = function()
 	local telescope = require("telescope")
@@ -56,15 +33,50 @@ local config = function()
 })
 
   telescope.load_extension("fzf")
+	-- See `:help telescope.builtin`
+	local builtin = require 'telescope.builtin'
+	vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+	vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+	vim.keymap.set('n', '<leader>t', builtin.find_files, { desc = '[S]earch [F]iles' })
+	vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+	vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+	vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+	vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+	vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+	vim.keymap.set('n', '<leader>so', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+	vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+	-- Slightly advanced example of overriding default behavior and theme
+	vim.keymap.set('n', '<leader>/', function()
+		-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+		builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+			winblend = 10,
+			previewer = false,
+		})
+	end, { desc = '[/] Fuzzily search in current buffer' })
+
+	-- It's also possible to pass additional configuration options.
+	--  See `:help telescope.builtin.live_grep()` for information about particular keys
+	vim.keymap.set('n', '<leader>s/', function()
+		builtin.live_grep {
+			grep_open_files = true,
+			prompt_title = 'Live Grep in Open Files',
+		}
+	end, { desc = '[S]earch [/] in Open Files' })
+
+	-- Shortcut for searching your Neovim configuration files
+	vim.keymap.set('n', '<leader>sn', function()
+		builtin.find_files { cwd = vim.fn.stdpath 'config' }
+	end, { desc = '[S]earch [N]eovim files' })
+
 end
 
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-	keys = keys,
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
 	config = config,
 }
