e120df6f0d264082eb84ae89c2fab9708667fe86
vecin2 <vecin2@gmail.com>
2025-01-23 00:38:40 +0100

    add habamax and tokyonight colorschemes

--- files/nvim/lua/plugins/colorscheme.lua
+++ files/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,11 @@
 return {
-	-- "folke/tokyonight.nvim",
-	-- priority = 1000,
-	-- config = function()
-	--   local transparent = false -- set to true if you would like to enable transparency
-	--
-	--
-	--   require("tokyonight").setup({
-	--     style = "night",
-	--     transparent = transparent,
-	--     styles = {
-	--       sidebars = transparent and "transparent" or "dark",
-	--       floats = transparent and "transparent" or "dark",
-	--     },
-	--   })
-	--
-	--   vim.cmd("colorscheme tokyonight")
-	-- end,
+  {
+    'LunarVim/darkplus.nvim',
+    priority = 1000,
+    init = function()
+      vim.cmd [[colorscheme darkplus]]
+    end,
+  },
+	{ "ntk148v/habamax.nvim", dependencies={ "rktjmp/lush.nvim" } },
+	'folke/tokyonight.nvim',
 }
