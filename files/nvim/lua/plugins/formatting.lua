ed6fd31db126e23e2a6f8af11f07e3872f034157
vecin2 <vecin2@gmail.com>
2025-03-14 07:05:20 +0100

    use prettier on the project if avaialable, if not default to mason

--- files/nvim/lua/plugins/formatting.lua
+++ files/nvim/lua/plugins/formatting.lua
@@ -1,3 +1,28 @@
+local function get_prettier_command(mode, filename)
+	if mode == "local" then
+		local root_dir = vim.fs.root(filename, { "package.json", "node_modules" })
+		if root_dir then
+			local prettier_cmd = root_dir .. "/node_modules/.bin/prettier"
+			vim.notify("Using local Prettier: " .. prettier_cmd, vim.log.levels.WARN)
+			return prettier_cmd
+		else
+			vim.notify("⚠ No local Prettier found! Falling back to global Prettier.", vim.log.levels.WARN)
+			return nil
+		end
+	elseif mode == "mason" then
+		if vim.fn.executable("prettier") == 1 then
+			vim.notify("Using Mason/global Prettier", vim.log.levels.WARN)
+			return "prettier"
+		else
+			vim.notify(
+				"❌ Mason/global Prettier not found! Install it with `:MasonInstall prettier` or `npm install -g prettier`.",
+				vim.log.levels.ERROR
+			)
+			return nil
+		end
+	end
+end
+
 return { -- Autoformat
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
@@ -36,24 +61,40 @@ return { -- Autoformat
 				args = { "--fix", "--nocolor", vim.fn.expand("%:p") },
 				stdin = false, -- ansible-lint requires files as input
 			},
+			prettier_local = {
+				command = function(_, ctx)
+					return get_prettier_command("local", ctx.filename)
+				end,
+				args = function(_, ctx)
+					return { "--stdin-filepath", ctx.filename }
+				end,
+			},
+			prettier_mason = {
+				command = function(_, _)
+					return get_prettier_command("mason")
+				end,
+				args = function(_, ctx)
+					return { "--stdin-filepath", ctx.filename }
+				end,
+			},
 		},
 		formatters_by_ft = {
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- TODO: try prettierd which seem to not work - javascript = { "prettierd", "prettier", stop_after_first = true },
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			javascriptreact = { "prettier" },
-			typescriptreact = { "prettier" },
-			svelte = { "prettier" },
-			css = { "prettier" },
-			scss = { "prettier" },
-			html = { "prettier" },
-			htmlangular = { "prettier" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
-			graphql = { "prettier" },
-			liquid = { "prettier" },
+			javascript = { "prettier_local", "prettier_mason", stop_after_first = true },
+			typescript = { "prettier_local", "prettier_mason", stop_after_first = true },
+			javascriptreact = { "prettier_local", "prettier_mason", stop_after_first = true },
+			typescriptreact = { "prettier_local", "prettier_mason", stop_after_first = true },
+			svelte = { "prettier_local", "prettier_mason", stop_after_first = true },
+			css = { "prettier_local", "prettier_mason", stop_after_first = true },
+			scss = { "prettier_local", "prettier_mason", stop_after_first = true },
+			html = { "prettier_local", "prettier_mason", stop_after_first = true },
+			htmlangular = { "prettier_local", "prettier_mason", stop_after_first = true },
+			json = { "prettier_local", "prettier_mason", stop_after_first = true },
+			yaml = { "prettier_local", "prettier_mason", stop_after_first = true },
+			markdown = { "prettier_local", "prettier_mason", stop_after_first = true },
+			graphql = { "prettier_local", "prettier_mason", stop_after_first = true },
+			liquid = { "prettier_local", "prettier_mason", stop_after_first = true },
 			lua = { "stylua" },
 			python = { "isort", "black" },
 		},
