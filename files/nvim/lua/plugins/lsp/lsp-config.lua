4c804fd3471edb310023d0d8f79c0dd7b6f4c0f8
vecin2 <vecin2@gmail.com>
2025-06-20 11:55:53 +0200

    align mason config with updated version

--- files/nvim/lua/plugins/lsp/lsp-config.lua
+++ files/nvim/lua/plugins/lsp/lsp-config.lua
@@ -113,18 +113,6 @@ return {
 		-- , "prettierd", "prettier", "eslint_d" they are too new for tsc 4.7.4
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-		require("mason-lspconfig").setup({
-			handlers = {
-				function(server_name)
-					local server = servers[server_name] or {}
-					-- This handles overriding only values explicitly passed
-					-- by the server configuration above. Useful when disabling
-					-- certain features of an LSP (for example, turning off formatting for ts_ls)
-					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
-				end,
-			},
-		})
 
 		-- require("lspconfig").angularls.setup({
 		-- 	cmd = cmd,
